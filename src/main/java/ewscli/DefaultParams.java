d2a18a184af8b06f2437142abebdf940be6f2104
greymd <yamada@gr3.ie>
2021-07-13 11:11:40 +0100

    Use LOCALAPPDATA in windows

--- src/main/java/ewscli/DefaultParams.java
+++ src/main/java/ewscli/DefaultParams.java
@@ -8,7 +8,8 @@ public class DefaultParams {
     public static final String APPNAME = "ewscli";
     public static String defaultConfigPath () {
         if(Platform.isWindows()) {
-            return String.join(File.separator, new String[]{System.getProperty("user.home") , APPNAME, "config"});
+            // return String.join(File.separator, new String[]{System.getProperty("user.home") , APPNAME, "config"});
+            return String.join(File.separator, new String[]{System.getenv("LOCALAPPDATA") , APPNAME, "config"});
         }
         // Follow "XDG Base Directory" in *NIX
         return String.join(File.separator, new String[]{System.getProperty("user.home") ,".config", APPNAME, "config"});
