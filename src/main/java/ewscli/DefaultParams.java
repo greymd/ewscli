5bc5b63a0851cc436ff3b4b3d53cb88aee3e8bdf
greymd <yamada@gr3.ie>
2021-07-15 01:49:07 +0100

    Store config to under ~/.config in UNIX

--- src/main/java/ewscli/DefaultParams.java
+++ src/main/java/ewscli/DefaultParams.java
@@ -1,10 +1,16 @@
 package ewscli;
 
+import com.sun.jna.Platform;
+
 import java.io.File;
 
 public class DefaultParams {
     public static final String APPNAME = "ewscli";
     public static String defaultConfigPath () {
-        return String.join(File.separator, new String[]{System.getProperties().getProperty("java.home"), APPNAME + ".config"});
+        if(Platform.isWindows()) {
+            return String.join(File.separator, new String[]{System.getProperties().getProperty("java.home"), APPNAME + ".config"});
+        }
+        // Follow "XDG Base Directory" in *NIX
+        return String.join(File.separator, new String[]{System.getProperty("user.home") ,".config", APPNAME, APPNAME + ".config"});
     }
 }
